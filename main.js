b5a84d6168a18f7e719f0ae082337cb984c05eb2
Viper-X0 <vbcoc18@gmail.com>
2023-07-03 18:00:00 +0530

    ᴜᴘᴅᴀᴛᴇ 🔹

--- main.js
+++ main.js
@@ -1,127 +1,115 @@
 let express = require("express");
 let app = express();
-let { toBuffer } = require("qrcode");
+const fs = require ("fs-extra")
+let {
+    toBuffer
+} = require("qrcode");
 const CryptoJS = require("crypto-js");
 const {
-  default: makeWASocket,
-  useSingleFileAuthState,
-  Browsers,
-  delay,
-} = require("@adiwajshing/baileys");
+    delay,
+    useMultiFileAuthState,
+    BufferJSON,
+    fetchLatestBaileysVersion,
+    Browsers,
+    default: makeWASocket
+    } = require("@whiskeysockets/baileys")
+    const pino = require("pino");
+    let PORT = process.env.PORT || 3030;
+    const PastebinAPI = require("pastebin-js"),
+    pastebin = new PastebinAPI("h4cO2gJEMwmgmBoteYufW6_weLvBYCqT");
 
-const pino = require("pino");
-let PORT = process.env.PORT || 3030;
+    app.use("/", (req, res) => {
 
-const PastebinAPI = require("pastebin-js"),
-  pastebin = new PastebinAPI("h4cO2gJEMwmgmBoteYufW6_weLvBYCqT");
-app.use("/", (req, res) => {
-  const authfile = `./tmp/${makeid()}.json`;
-  const { state } = useSingleFileAuthState(authfile, pino({ level: "silent" }));
-  function Xasena() {
-    try {
-      let session = makeWASocket({
-        auth: state,
-        printQRInTerminal: true,
-        logger: pino({ level: "silent" }),
-        browser: Browsers.macOS("Desktop"),
-        downloadHistory: false,
-        syncFullHistory: false,
-      });
+        async function XAsena() {
 
-      session.ev.on("connection.update", async (s) => {
-        if (s.qr) {
-          res.end(await toBuffer(s.qr));
+            try {
+                let {
+                    version, isLatest
+                } = await fetchLatestBaileysVersion()
+                const {
+                    state, saveCreds
+                } = await useMultiFileAuthState(`./session`)
+                const session = makeWASocket({
+                    logger: pino({
+                        level: 'silent'
+                    }),
+                    printQRInTerminal: false,
+                    browser: Browsers.macOS("Desktop"),
+                    auth: state,
+                    version
+                })
+                //------------------------------------------------------
+
+                session.ev.on("connection.update", async (s) => {
+                    if (s.qr) {
+                        res.end(await toBuffer(s.qr));
+                    }
+                    const {
+                        connection,
+                        lastDisconnect
+                    } = s
+                    if (connection == "open") {
+                        const authfile = (`./session/creds.json`)
+                        await delay(1000 * 10)
+
+                        let link = await pastebin.createPasteFromFile(
+                            authfile,
+                            "XAsena",
+                            null,
+                            0,
+                            "N"
+                        );
+                        let data = link.replace("https://pastebin.com/", "");
+                        let code = btoa(data);
+                        var words = code.split("");
+                        var ress = words[Math.floor(words.length / 2)];
+                        let c = code.split(ress).join(ress + "_XASENA_");
+                        await session.sendMessage(session.user.id, {
+                            text: `${c}`
+                        })
+                        await session.sendMessage(session.user.id, {
+                            text: `\n*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*
+
+                            ◕ ⚠️ *ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*`
+                        })
+
+                        await session.sendMessage(session.user.id, {
+                            document: {
+                                url: authfile
+                            },
+                            fileName: "creds.json",
+                            mimetype: "application/json",
+                        });
+                        await fs.rm('./session', {
+                            recursive: true, force: true
+                        })
+                        process.exit(0)
+                    }
+                    if (
+                        connection === "close" &&
+                        lastDisconnect &&
+                        lastDisconnect.error &&
+                        lastDisconnect.error.output.statusCode != 401
+                    ) {
+                        XAsena()
+                    }
+                })
+                session.ev.on('creds.update',
+                    saveCreds)
+                await delay(3000 * 10);
+                session.ev.on("messages.upsert",
+                    () => {})
+
+            }catch(err) {
+                console.log(
+                    err + "Unknown Error Occured Please report to Owner and Stay tuned"
+                );
+            }
+
+
         }
-        const { connection, lastDisconnect } = s;
-        if (connection == "open") {
-          await delay(500 * 10);
-          let link = await pastebin.createPasteFromFile(
-            authfile,
-            "Millie-MD session",
-            null,
-            0,
-            "N"
-          );
-          let data = link.replace("https://pastebin.com/", "");
-          let code = btoa(data);
-          var words = code.split("");
-          var ress = words[Math.floor(words.length / 2)];
-          let c = code.split(ress).join(ress + "_XASENA_");
+        XAsena()
 
-          const templateButtons = [
-            {
-              index: 1,
-              urlButton: {
-                displayText: "Copy Code",
-                url: `https://www.whatsapp.com/otp/copy/${c}`,
-              },
-            },
-            {
-              index: 2,
-              urlButton: {
-                displayText: "Github",
-                url: `github.com/Neeraj-x0/Millie-MD`,
-              },
-            },
-          ];
+    })
 
-          const templateMessage = {
-            text: `\nᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ
-          
-◕ ⚠️ *ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*`,
-            footer: "sᴇssɪᴏɴ",
-            templateButtons: templateButtons,
-          };
-
-          await session.sendMessage(session.user.id, templateMessage);
-          await session.sendMessage(session.user.id, {
-            document: { url: authfile },
-            fileName: "session.json",
-            mimetype: "application/json",
-          });
-
-          await delay(3000 * 10);
-          process.send("reset");
-        }
-        if (
-          connection === "close" &&
-          lastDisconnect &&
-          lastDisconnect.error &&
-          lastDisconnect.error.output.statusCode != 401
-        ) {
-          Xasena();
-        }
-      });
-    } catch (err) {
-      console.log(
-        err + "Unknown Error Occured Please report to Owner and Stay tuned"
-      );
-    }
-  }
-
-  Xasena();
-});
-app.listen(PORT, () => console.log("App listened on port", PORT));
-
-const encrypt = (text) => {
-  return CryptoJS.AES.encrypt(text, (passphrase = "123")).toString();
-};
-
-const decrypt = (text) => {
-  return CryptoJS.AES.decrypt(text, passphrase).toString();
-};
-
-function makeid(num = 9) {
-  var result = "";
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
-}
-
-let encode = (f) => {
-  return f.replace("=", "");
-};
+    app.listen(PORT, () => console.log("App listened on port", PORT));
