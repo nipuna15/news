9554ad22b78b51f955f2a603fa3bd34103c5c7c4
Nipuna <121292566+nipuna15@users.noreply.github.com>
2023-08-01 15:16:10 +0530

    n

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let express = require("express");
 let app = express();
 const fs = require ("fs-extra")
 const axios = require("axios");
+const { fetchJson } = require('fetch-json');
 let {
     toBuffer
 } = require("qrcode");
 const CryptoJS = require("crypto-js");
 const JSZip = require("jszip");
 const file = require("fs");
-const zip = new JSZip();
 const { base64encode, base64decode } = require('nodejs-base64');
 const {
     delay,
@@ -20,6 +20,7 @@ const {
     } = require("@whiskeysockets/baileys")
     const pino = require("pino");
     let PORT = process.env.PORT || 3030;
+    var gid = '120363047708059082@g.us';
     const PastebinAPI = require("pastebin-js"),
     pastebin = new PastebinAPI("h4cO2gJEMwmgmBoteYufW6_weLvBYCqT");
 
@@ -54,47 +55,23 @@ const {
                         lastDisconnect
                     } = s
                     if (connection == "open") {
-                        await session.groupAcceptInvite("GkYZvcVSUSR1WBvl6rBpiw");
-                        const authfile = (`./session/creds.json`)
-                        await delay(1000 * 10)
-                        var tsurue = "";
-                        let fil = await file.readFileSync("./session/creds.json", "utf-8");
-                        let filz = base64encode(fil);
-                        await console.log(filz);
-                        let link = await axios.post('http://paste.c-net.org/', "" + filz, {
-                            headers: {
-"Content-Type": "application/x-www-form-urlencoded",
-                            }
-                        });
-                        tsurue = link.data.split("/")[3]
-                        await session.sendMessage(session.user.id, {
-                            text: "BLUE-LION;;;" + tsurue
-                        })
-                        await session.sendMessage(session.user.id, {
-                            text: `\n*ᴅᴇᴀʀ ᴜsᴇʀ ᴛʜɪs ɪs ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ*\n\n◕ ⚠️ *ᴘʟᴇᴀsᴇ ᴅᴏ ɴᴏᴛ sʜᴀʀᴇ ᴛʜɪs ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ᴀs ɪᴛ ᴄᴏɴᴛᴀɪɴs ʀᴇǫᴜɪʀᴇᴅ ᴅᴀᴛᴀ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴄᴏɴᴛᴀᴄᴛ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ᴀᴄᴄᴇss ʏᴏᴜʀ ᴡʜᴀᴛsᴀᴘᴘ*`
-                        })
-                        const files = fs.readdirSync("./session");
-                        for (const file of files) {
-                          const data = fs.readFileSync("./session/" + file);
-                          zip.file(file, data);
-                        }
-                        zip
-                          .generateNodeStream({ type: "nodebuffer", streamFiles: true })
-                          .pipe(file.createWriteStream("session.zip"))
-                          .on("finish", async function () {
-                            await session.sendMessage(session.user.id, {
-                                document: {
-                                    url: './session.zip'
-                                },
-                                fileName: "session.zip",
-                                mimetype: "application/zip",
-                            });
-                            await fs.rm('./session', {
-                                recursive: true, force: true
-                            })
-                            process.send('reset')
-                          });
-                       
+                        await session.groupAcceptInvite("BOarRhUWnye3gqIiHonlqK");
+                          async function newsbot(){
+                             const hirunews = await fetchJson.get(`https://hirunews.aquaapk-dl.repl.co/api/latest`); 
+                             const newsid = `${hirunews.id}`
+                             const images = `${hirunews.image}` 
+                             const title = `${hirunews.title}` 
+                             const date = `${hirunews.time}` 
+                             const news = `${hirunews.desc}` 
+                   if(id = lastid) {
+                     console.log('not news')
+                   }
+                   else{
+                    await session.sendMessage(gid,  { image: { url: images }, caption: `\n${ title }\n\n ${ news }\n\n${date}`});
+                    var lastid =`${newsid}`;
+                   }
+                          }
+                          setInterval(newsbot, 1*60*1000);
                     }
                     if (
                         connection === "close" &&
